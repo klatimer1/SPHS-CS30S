bf3cf5a4605abb49dda41cad25c08aeeae297faf
Mrs. Latimer <46603910+klatimer1@users.noreply.github.com>
2024-04-21 10:59:45 -0500

    Add files via upload

---
+++ ForLoopDemo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.forloopdemo;
+
+/**
+ * For Loop Demo
+ * @author klatimer
+ */
+public class ForLoopDemo {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++)
+        {
+            System.out.print(i + " ");
+        } //end for
+    } // end main
+} // end class
